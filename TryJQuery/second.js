93576f948d8a336db7563107b7551d8a6aa3ee4f
hedgehogus <research162@gmail.com>
2017-09-04 23:48:48 +0300

    descendant and child selector

--- TryJQuery/second.js
+++ TryJQuery/second.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
-    $("li").text("new point");
+    //$("li").text("new point");
     $(".promo").css("background-color", "#aaaaaa");
     $("ul").css("width", "300px");
     $("#destinations").css("text-transform", "uppercase");
+
+    // descendant selector
+    $("#destinations li").css("color","#ff0000");
+
+    // child selector
+    $("#destinations > li").css("color","#2200ff");
 });
