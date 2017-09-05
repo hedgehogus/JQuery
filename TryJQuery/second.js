d4bc629f0e3a891a27191432ea7a58f0194b8252
hedgehogus <research162@gmail.com>
2017-09-05 05:06:48 +0300

    traversing the dom

--- TryJQuery/second.js
+++ TryJQuery/second.js
@@ -20,4 +20,27 @@ $(document).ready(function(){
     $("#destinations li:odd");
     // even positions
     $("#destinations li:even");
+
+    //filtering by traversing
+    //it takes a bit more code, but its faster
+    $("#destinations").find(li);
+
+    $("li:first");
+    $("li").first();
+
+    $("li:last");
+    $("li").last();
+
+    //walking the dome
+    //method chaining
+    $("li").first().next();
+    // go back to the first item
+    $("li").first().next().prev();
+
+    //get parent of selected element
+    $("li").first().parent();
+
+    //get only the direct children
+    $("#destinations").children("li");
+    //children(), unlike find(), only selects direct children
 });
