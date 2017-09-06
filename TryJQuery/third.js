706cc014309ca4cc77e9163abf02648065e7fa91
hedgehogus <research162@gmail.com>
2017-09-06 03:44:30 +0300

    listener

--- TryJQuery/third.js
+++ TryJQuery/third.js
@@ -1,18 +1,28 @@
 $(document).ready(function(){
     // create a <p> node with the price
-    var price =$('<p>From $399.99</p>');
+    var pr =$('<p>From $399.99</p>');
     // create a node but doesnt add it to the DOM
 
     //.append(<element>) /last child
     //.prepend(<element>) /first child
     //.after(<element>)
     //.before(<element>)
+    var button = $("button").css("width","200px");
+    $("button").on("click", function(){
+        var price =$('<p>From $399.99</p>');
 
-    $(".vacation").append(price);
-    $("button").remove();
+        price.css("width", "200px");
+        price.css("background-color", "#ddd");
+        price.css("text-align", "center");
+        
+        $(".vacation").append(price);
+        $("button").remove();
+    });
+
+   
 
     //.appendTo(<element>) 
-    // $(".vacation").append(price) ===  price.append($(".vacation"));
+    // $(".vacation").append(price) ===  price.appendTo($(".vacation"));
     //.prependTo(<element>) 
     //.insertAfter(<element>)
     //.insertBefore(<element>)
