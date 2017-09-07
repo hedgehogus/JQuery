fa56d71dc062c47ef8de4e1323a4764ce8769c22
hedgehogus <research162@gmail.com>
2017-09-07 06:42:02 +0300

    traversing and filtering

--- TryJQuery/third.js
+++ TryJQuery/third.js
@@ -8,8 +8,10 @@ $(document).ready(function(){
     //.after(<element>)
     //.before(<element>)
     var button = $("button").css("width","200px");
-    $("button").on("click", function(){
-        var price =$('<p>From $399.99</p>');
+    $(".vacation").on("click","button", function(){ // event delegation
+        var vacation = $(this).closest(".vacation");
+        var amount = vacation.data("price");
+        var price =$(`<p>From $${amount}</p>`);
 
         price.css("width", "200px");
         price.css("background-color", "#ddd");
@@ -18,28 +20,42 @@ $(document).ready(function(){
         // $(this).after(price);
         // $(this).parent().parent().append(price);
         // $(this).parents('.vacation').append(price); // all ancestors nodes
-        $(this).closest('.vacation').append(price); //one node
+        vacation.append(price); //one node
         $(this).remove();
     });
 
     //or
     
     $("li").on("click", function(){
-        var price =$('<p>From $399.99</p>');
+        var data = $(this).data("price");
+        var price =$(`<p>From $${data}</p>`);
 
         price.css("width", "200px");
         price.css("background-color", "#ddd");
         price.css("text-align", "center");        
-        
-        $(this).append(price); 
-        $(this).find("button").remove(); //find child
+                
+        // $(this).append(price); 
+        // $(this).find("button").remove(); //find child
     });
-
-   
-
+  
     //.appendTo(<element>) 
     // $(".vacation").append(price) ===  price.appendTo($(".vacation"));
     //.prependTo(<element>) 
     //.insertAfter(<element>)
     //.insertBefore(<element>)
+
+    $("#filters").on("click",".onsale-filter",function(){
+        // find all vacations that are on-sale
+        // add a class to these vacations       
+        $('.highlighted').removeClass("highlighted");
+        $(".vacation").filter(".onsale").find("h2").addClass("highlighted");
+        //$(".vacation").append($("<p>highlighted</p>"))
+    })
+
+    $("#filters").on("click",".expiring-filter",function(){
+        // find all vacations that are on-sale
+        // add a class to these vacations
+        $(".highlighted").removeClass("highlighted");
+        $(".vacation").filter(".expiring").children("h2").addClass("highlighted");
+    })
 });
