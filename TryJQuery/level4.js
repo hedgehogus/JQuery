980b58a865f6d6542f9438f618944ac1358d8b06
hedgehogus <research162@gmail.com>
2017-09-11 19:58:08 +0300

    mouse events

--- TryJQuery/level4.js
+++ TryJQuery/level4.js
@@ -6,6 +6,27 @@ $(document).ready(function(){
     })
 
     console.log($('li').length); // to query how many nodes are on a page
-    console.log($('ul').length);
-    console.log($('button').length);
+   // console.log($('ul').length);
+   // console.log($('button').length);
+
+    $(".hover").on("mouseenter","h1",function(){
+       $(this).closest(".hover").find("section").slideDown();
+    });
+    $(".hover").on("mouseleave","h1",function(){
+       $(this).closest(".hover").find("section").slideUp();
+    });  
+
+   // mouse events
+   // click
+   // dbclick
+   // focusin
+   // focuseout
+   // mousedown
+   // mouseup
+   // mousemove
+   // mouseout
+   // mousenter
+   // mouseleave
+   // mouseover
+ 
 });
