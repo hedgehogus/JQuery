53c9d8f08632e841bb461a9c1462afd9eb5e1831
hedgehogus <research162@gmail.com>
2017-09-21 04:05:07 +0300

    animation

--- TryJQuery/level5.js
+++ TryJQuery/level5.js
@@ -18,14 +18,27 @@ $(document).ready(function(){
 
     //    $(this).closest("#vacations").find(".vacation").css(paramDefault).find(".price").hide();
         
-    //    let param = {
-    //        "background-color": "#778899",
-    //        "border-color": "#333333"
-    //    };
+        let param = {
+            "background-color": "#778899",
+            "border-color": "#333333"
+        };
     //     $(this).css(param).find(".price").show();
     
     
     $(this).toggleClass('highligted');
+
+    //Adding Movement
+    // if ($(this).hasClass('highligted')){
+    //     $(this).animate({"margin-top": "+20px"}, 1000);
+    // } else {
+    //     $(this).animate({"margin-top": "0px"}, 1000);
+    // }
+    
+    // default - 400ms
+    // fast - 200ms
+    // slow - 600ms
+    // we can give specific speed as a string or in miliseconds 
+    // to slideToggle() or fadeToggle()  
 
  });
 
