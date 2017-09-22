7374d871317663c20a2572be67511f8c9e8cb577
hedgehogus <research162@gmail.com>
2017-09-22 19:14:35 +0300

    ajax options

--- JQuery/part1.js
+++ JQuery/part1.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 
     // with ajax
     $(".confirmation").on("click","button",function(){
-    let url="confirmation.html";
+    let url="https://www.instagram.com/lyuda_and_hedgehog/";
     
        $.ajax(url, {
         // or can be relative url
@@ -25,7 +25,7 @@ $(document).ready(function(){
             // in html <div class="ticket data-confNum="1234">
             // and
             // data: {"confNum":$(".ticket").data("confNum")} 
-        })
+        });
 
         // or
 
@@ -35,6 +35,35 @@ $(document).ready(function(){
 
 
         $(this).closest(".confirmation").find(".ticket").slideDown();
+
+        // WHAT TO DO WHEN OUR REQUEST IS NOT SUCCESFUL
+
+         $.ajax(url, {
+            success: function(data){        
+                $(".ticket").html(response).slideDown();
+            },
+            //data: {"confNum":1234},
+            error: function(request, errorType, errorMessage){
+                alert("error: " + errorType + " with message: " + errorMessage);
+            },
+            // other parameters
+            timeout:3000, // defaults depends of browser
+            beforeSend: function(){
+                $(".confirmation").addClass('is-loading');
+                // good place to trigger a spiner
+            },
+            complete: function(){
+                 $(".confirmation").removeClass('is-loading');
+                // good place for turn of that loading screen
+                // runs after both succes and error
+            }
+            
+        });
+    // this code doesnt look for change inside confirmation class, like adding .view-boarding-pass
+    $(".confirmadion .view-boarding-pass").on("click", function(){});
+    // and this will work and find class after loading from other request
+    $(".confirmation").on('click', ".view-boarding-pass", function(){});
+
     });
 
 
