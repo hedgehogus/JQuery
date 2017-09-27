7a8f128ae95d6978705c2b9a3406295a2c91dcbd
hedgehogus <research162@gmail.com>
2017-09-27 05:51:54 +0300

    json

--- JQuery/part3.js
+++ JQuery/part3.js
@@ -24,6 +24,53 @@ function CreateForm(el){
                 form.remove();
                 $("#vacation").hide().html(result).fadeIn();
             }
+        });
+
+        // JSON Query
+
+        $.ajax($('form').attr("action"), {
+            type:"POST",
+            data:form.serialize(),
+            dataType: "json", // parse the response as Json
+            success: function(result){
+                form.remove();
+
+                var msg = $("<p></p>");
+                msg.append("Destination: " + result.location + ". ");
+                msg.append("Price: " + result.totalPrice + ". ");
+                msg.append("Nights: " + result.nights + ". ");
+                msg.append("Confirmation: " + result.confirmation + ". ");
+
+                $("#vacation").hide().html(msg).fadeIn();
+            },
+            contentType: "application/json" // ask the server to respond with JSON
         })
+
+        // example
+
+         $.ajax('/book', {
+            type: 'POST',
+             data: $('form').serialize(),
+            dataType: 'json',
+            success: function(response) {
+                $('.tour').html('<p></p>')
+                     .find('p')
+                     .append('Trip to ' + response.description)
+                     .append(' at $' + response.price)
+                     .append(' for ' + response.nights + ' nights')
+                     .append('. Confirmation: ' + response.confirmation);
+      }
     });
+    });
+}
+
+// JSON - Java Script Object Notation
+
+// Server returns details, we can use it as we wish
+
+ let a = { 
+    totalPrice: 2196.00,
+    nights: 4,
+    location: "paris, france",
+    confirmation: "345feab"
 }
